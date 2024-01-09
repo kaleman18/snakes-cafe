e990e4d175057a07a8d1c251bb26241d191fea73
kaleman18 <kylealeman18@gmail.com>
2024-01-08 16:08:06 -0800

    start

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -35,5 +35,15 @@ Unicorn Tears
 ** What would you like to order? **
 ***********************************""")
 order = input('> ')
+ordernumber = 1
+
+while order != 'quit':
+    order = input('> ')
+    if ordernumber == 1:
+        print(f'**{ordernumber} order of {order} has been added to your meal **')
+    elif ordernumber > 1:
+        print(f'**{ordernumber} orders of {order} have been added to your meal**')
+    ordernumber +=1
+
 
 
